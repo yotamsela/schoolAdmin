95c75f6c5e9f8e09c027d695a6bfb4e77cda6488
Yotam Sela <yotamsela@gmail.com>
2014-04-17 23:16:57 +0300

    added teachersdata, and got most of the data out in the main

--- SchoolAdministrator/src/techersjob/Main.java
+++ SchoolAdministrator/src/techersjob/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -16,44 +17,58 @@ import bagruyot.ExcellReaderAndWriter;
 import bagruyot.Writer;
 
 public class Main {
-	
+
 	enum HATIVA {UPPER, MEDIUM}
 	private static final int TEACHER_AGE_ROW = 8;
 	private static final int TEACHER_AGE_COLUMN = 8;
-	
-	
+
+
 	public static void main(String[] args) throws IOException {
 		System.out.println("start");
 		String pathname = "temp.xlsx";
 		File file = new File(pathname);
-//		OutputStream out = new BufferedOutputStream(new FileOutputStream(outF));
-//		XSSFWorkbook newWorkbok = new XSSFWorkbook();
-		
+		//		OutputStream out = new BufferedOutputStream(new FileOutputStream(outF));
+		//		XSSFWorkbook newWorkbok = new XSSFWorkbook();
+
 		XSSFWorkbook workbook = ExcellReaderAndWriter.openXlsxFile(file);
 		XSSFSheet sheet = workbook.getSheetAt(1);
 		HATIVA hativaType = HATIVA.UPPER;
 		double frontalSum = getFrontalSum(sheet,hativaType);
 		double gmolSum = getGmolSum(sheet,hativaType);
 		int teachersAge = getTeacherAge(sheet);
-		
-		
+		boolean perticipateinozTmura = getIsPerticipatesInosTmura(sheet);
+		boolean isMothertoChileover14 = getIsMothertoChileover14(sheet); 
+
+
 		System.out.println("frontal: "+frontalSum);
 		System.out.println("gmol: "+gmolSum);
 		System.out.println("age: "+teachersAge);
-		//xlsxFile.createSheet("hello2");
-		//XSSFSheet sheet = workbook.createSheet("hello");
-//		Writer.createCell(workbook, sheet.createRow(0), 0, "100", XSSFCellStyle.BORDER_MEDIUM, XSSFCellStyle.BORDER_MEDIUM, XSSFCellStyle.BORDER_MEDIUM, 
-//				XSSFCellStyle.BORDER_THIN, true, (short)11, true, false);
-		
+		System.out.println("participates in Oz Tmura: "+perticipateinozTmura);
+		System.out.println("mother to child over 14: "+isMothertoChileover14);
+
 		Writer.write(workbook, "temp.xlsx");
 		System.out.println("end");
 	}
-	
-	
-//	private static double calculatePartani(int hours, HATIVA hativaType){
-//		
-//	}
-	
+
+
+	private static boolean getIsMothertoChileover14(XSSFSheet sheet) {
+		XSSFRow row = sheet.getRow(TEACHER_AGE_ROW+1);
+		XSSFCell cell = row.getCell(TEACHER_AGE_COLUMN);
+		return (cell.getStringCellValue().equals("λο"))? true:false;
+	}
+
+
+	private static boolean getIsPerticipatesInosTmura(XSSFSheet sheet) {
+		XSSFRow row = sheet.getRow(TEACHER_AGE_ROW+2);
+		XSSFCell cell = row.getCell(TEACHER_AGE_COLUMN);
+		return (cell.getStringCellValue().equals("λο"))? true:false;
+	}
+
+
+	//	private static double calculatePartani(int hours, HATIVA hativaType){
+	//		
+	//	}
+
 	private static int getHativaRow(HATIVA hativaType){
 		int row = -1;
 		switch(hativaType){
@@ -65,26 +80,25 @@ public class Main {
 		}	
 		return row;
 	}
-	
+
 	private static int getTeacherAge(XSSFSheet sheet){
 		XSSFRow row = sheet.getRow(TEACHER_AGE_ROW);
 		XSSFCell cell = row.getCell(TEACHER_AGE_COLUMN);
-		String date = cell.toString();
-		DateFormat df = new SimpleDateFormat("dd-yyyy-mmm");
-		try {
-			Date birthDate = df.parse(date);
-			double yearDiff = new Date().getYear() - birthDate.getYear();
-			double monthsDiff = new Date().getMonth() - birthDate.getMonth();
-			return (int)(yearDiff*12 + monthsDiff)/12;
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Date dateCellValue = cell.getDateCellValue();
+		Calendar birthDayCal = Calendar.getInstance();
+		birthDayCal.setTime(dateCellValue);
+		Calendar currentCal = Calendar.getInstance();
+		currentCal.setTime(new Date());
+
+		int yearOld = currentCal.get(Calendar.YEAR) - birthDayCal.get(Calendar.YEAR);
+		if(currentCal.get(Calendar.MONTH) < birthDayCal.get(Calendar.MONTH)){
+			yearOld--;
 		}
-		
-		
-		return -1;
+		return yearOld;
 	}
-	
+
+
+
 	/**
 	 * getting the frontal sum
 	 * 
@@ -99,8 +113,8 @@ public class Main {
 		double value = cell.getNumericCellValue();
 		return value;
 	}
-	
-	
+
+
 	/**
 	 * getting the gmol sum
 	 * 
---
+++ SchoolAdministrator/src/techersjob/TeacherData.java
@@ -0,0 +1,14 @@
+package techersjob;
+
+public class TeacherData {
+	
+	private double frontalHours;
+	private double gmol;
+	private int teacherAge;
+	private boolean ozTmura;
+	private boolean childOver14;
+	
+	
+	
+
+}
